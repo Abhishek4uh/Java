da8f20729e7ab9b2e29cb4ae177bdd9bc102d279
Abhishek Raj <48413563+Abhishek4uh@users.noreply.github.com>
2023-09-17 19:25:44 +0530

    Create Greeting.java
    User Greetings program

---
+++ Greeting.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter Your Name ");
+		String name =sc.next();
+	  System.out.println("How you doin "+ name +"?");
+	
+	}
+	
+}
