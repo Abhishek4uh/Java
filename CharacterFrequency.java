47c8fce3ec27bbd8e0631a2ac926d95217783711
Abhishek Raj <48413563+Abhishek4uh@users.noreply.github.com>
2023-10-26 20:50:08 +0530

    CharacterFrequency.java
    added the program

---
+++ CharacterFrequency.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main
+{
+	public static void main(String[] args) {
+		String name= "HelloWorld";
+		HashMap<Character,Integer> record= new HashMap<>();
+		char [] allchars= name.toCharArray();
+		for(char ch: allchars){
+		    if(record.containsKey(ch)){
+		        record.put(ch, record.get(ch) + 1);
+		    }
+		    else{
+		        record.put(ch,1);
+		    }
+		}
+		System.out.println(record);
+	}
+}
