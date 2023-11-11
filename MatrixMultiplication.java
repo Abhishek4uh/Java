c3064d4159889fc2c1a74dabfe921b3712c90956
Abhishek Raj <48413563+Abhishek4uh@users.noreply.github.com>
2023-11-12 01:02:42 +0530

    Update MatrixMultiplication.java

--- MatrixMultiplication.java
+++ MatrixMultiplication.java
@@ -3,9 +3,9 @@ import java.util.*;
 public class Main
 {
 	public static void main(String[] args) {
-		int A[][] = {{17,4,}, {17 , 16}};
-        int B[][] = {{9, 2}, {7, 1}};
-        //C[][] = {{181, 38}, {265, 50}}
+	int A[][] = {{17,4}, {17,16},{3,5}};
+        int B[][] = {{9,2,5}, {7,5,1}};
+        
         
         int col_A= A[0].length;
         int row_B= B.length;
@@ -18,11 +18,11 @@ public class Main
         
         int row_A= A.length;
         int col_B= B[0].length;
-        
+        //k->> common dimension...(col of A or row of B) 
         int res[][]= new int[row_A][col_B];
         for(int i=0;i<res.length;i++){
             for(int j=0;j<res[i].length;j++){
-                for(int k=0;k<2;k++){
+                for(int k=0;k<col_A;k++){
                     res[i][j]+=A[i][k]*B[k][j];
                 }
             }
