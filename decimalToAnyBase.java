049890f391cee9df1cbed3f41d0b3a0bcd03fc56
Abhishek Raj <48413563+Abhishek4uh@users.noreply.github.com>
2025-03-18 23:24:46 +0530

    Update decimalToAnyBase.java

--- decimalToAnyBase.java
+++ decimalToAnyBase.java
@@ -3,9 +3,18 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         
+        //decimalToAnyBase
         System.out.println(decimalToAnyBase(19,2));  //Binary
         System.out.println(decimalToAnyBase(19,8));  //Octal
         System.out.println(decimalToAnyBase(19,16)); //Hexa
+        
+        System.out.println("\n------\n");
+        //binaryToAnyBase
+        System.out.println(binaryToAnyBase(10011,2)); //base 2
+        System.out.println(binaryToAnyBase(101,8)); //base 8
+        System.out.println(binaryToAnyBase(101,10)); //base 10
+        System.out.println(binaryToAnyBase(101,16)); //base 16
+        
     }
 
     public static int decimalToAnyBase(int decimalNum, int base) {
@@ -19,4 +28,17 @@ public class Main {
        }
        return result;
     }
+    
+    public static int binaryToAnyBase(int binaryNum, int base) {
+       int result=0;
+       int power=0;
+       while(binaryNum>0){
+           int rem= binaryNum%10;
+           binaryNum= binaryNum/10;
+           result+= rem * Math.pow(base,power);
+           power++;
+       }
+       return result;
+    }
+    
 }
